9c69b4621f3e84732162ff98278214c2c13a9b7e
niujt <670482466@qq.com>
2019-05-13 16:14:16 +0800

    commit

--- src/main/java/com/wxthxy/market/SuperMarketApp.java
+++ src/main/java/com/wxthxy/market/SuperMarketApp.java
@@ -3,10 +3,17 @@ package com.wxthxy.market;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.wxthxy.market.dao")
-public class SuperMarketApp {
+public class SuperMarketApp extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SuperMarketApp.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SuperMarketApp.class,args);
     }
--- src/main/java/com/wxthxy/market/controller/TestController.java
+++ src/main/java/com/wxthxy/market/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @RequestMapping("/test")
     public String test(){
-        return "index";
+        return "test";
     }
 }
