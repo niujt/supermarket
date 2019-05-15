02c6ed9cf00b0f7839220f293a0c4b4f24e823be
niujiatao <niujiatao@tongyu.tech>
2019-05-15 10:43:49 +0800

    first commit

---
+++ src/main/java/com/wxthxy/supermarket/SuperMarketApp.java
@@ -0,0 +1,11 @@
+package com.wxthxy.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarketApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SuperMarketApp.class,args);
+    }
+}
